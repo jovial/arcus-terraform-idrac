15eb45cc422c81ee1449100327e0c07497f1a31b
Will Szumski <will@stackhpc.com>
2020-10-28 17:22:11 +0000

    Configure all mellanox interfaces

--- ansible/files/mlnx-update.sh
+++ ansible/files/mlnx-update.sh
@@ -11,11 +11,16 @@ LINK_TYPE_P2=2
 
 mst start && mlxfwmanager -u -i /root/fw-ConnectX6-rel-20_28_2006-MCX653106A-ECA_Ax-UEFI-14.21.17-FlexBoot-3.6.102.bin -y | grep -i reboot && export NEED_REBOOT=1
 
-ip a | grep -E '(p4p2|p4p1|ens.f0|ens.f1)' || {
-  mst start
-  mlxconfig -y -d /dev/mst/mt4123_pciconf0 set LINK_TYPE_P1=$LINK_TYPE_P1 LINK_TYPE_P2=$LINK_TYPE_P2
-  export NEED_REBOOT=1
-}
+for dev in /dev/mst/mt4123_pciconf[^.]; do
+  mlxconfig -y -d $dev query | grep LINK_TYPE_P1 | grep IB || {
+    mlxconfig -y -d $dev set LINK_TYPE_P1=$LINK_TYPE_P1 LINK_TYPE_P2=$LINK_TYPE_P2
+    export NEED_REBOOT=1
+  }
+  mlxconfig -y -d $dev query | grep LINK_TYPE_P2 | grep ETH || {
+    mlxconfig -y -d $dev set LINK_TYPE_P1=$LINK_TYPE_P1 LINK_TYPE_P2=$LINK_TYPE_P2
+    export NEED_REBOOT=1
+  }
+done
 
 if [ $NEED_REBOOT -eq 1 ]; then
   reboot
